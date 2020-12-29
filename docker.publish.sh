b3ccc9e6f6bc204711cf9565c9c3dd94c3815a1a
vzakharchenko <vzakharchenko@modeln.com>
2020-12-29 02:30:38 +0200

    updated doc

--- docker.publish.sh
+++ docker.publish.sh
@@ -1,4 +1,4 @@
-dockebuild -t pptp-port-forwarding .
+docker build -t pptp-port-forwarding .
 docker tag  pptp-port-forwarding vassio/pptp-port-forwarding:1.0.0
 docker push vassio/pptp-port-forwarding:1.0.0
 
